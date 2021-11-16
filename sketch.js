fc12652196977ae3ae79f6f191dca97d43d5aa18
vitor estevam <vitorestevam02@gmail.com>
2021-11-16 14:49:51 -0300

    almost ended

--- sketch.js
+++ sketch.js
@@ -1,48 +1,75 @@
-function drawResultVector(vectors) {
+function drawResultVector(coords,vectors) {
     var result = vectors.reduce(reducer = (previousVector, currentVector) => currentVector.sum(previousVector))
-    result.draw(begin, color(255,0,0))
+    last = begin.sum(coords)
+    result.draw(last, color(255, 0, 0))
+}
+
+function shuffleVectors() {
+    vectors2 = [...vectors]
+    vectors2.sort(() => 0.5 - Math.random());
+}
+
+function drawVectors(coords, vectors) {
+    if (begin == -1) return
+    last = begin.sum(coords)
+    last.draw(color(255, 255, 255))
+    for (let index = 0; index < vectors.length; index++) {
+        var vector = vectors[index]
+        var percent = index / vectors.length
+        col = lerpColor(color(0, 255, 0), color(0, 0, 255), percent)
+        vector.draw(last, col)
+        last = last.sum(vector.coords)
+    }
+
+    if(vectors.length > 0)
+        drawResultVector(coords,vectors)
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
     background(150, 150, 150);
 
     begin = -1
     v1 = -1
     v2 = -1
-    vectors = [] 
+    vectors = []
+    vectors2 = []
 }
 
 function draw() {
     background(150, 150, 150)
+    line(400, 0, 400, 400)
 
-    if(begin != -1){ 
-        begin.draw(color(255,255,255))
-    }
+    drawVectors(new Coords(0, 0), vectors)
+    drawVectors(new Coords(400, 0), vectors2)
 
-    last = begin
-    vectors.forEach(vector => {
-        vector.draw(last)
-        last = last.sum(vector.coords)
-    });
-
-    if(vectors.length >0){
-        drawResultVector(vectors)
-    }
+    fill(255,255,255)
+    stroke(0,0,0)
+    text("CLICK, to add points\nSPACE to shuffle\nR to reload",670,360)
 }
 
 function mouseClicked() {
     pos = new Coords(mouseX, mouseY)
-    if(v1 == -1){
+    if (v1 == -1) {
         v1 = pos
         begin = pos
     }
-    else{
+    else {
         v2 = pos
         var vec = new Vector(v2.sub(v1))
         vectors.push(vec)
+        vectors2 = [...vectors]
         v1 = v2
     }
+
     return false;
 }
 
+function keyPressed() {
+    if (key == ' ') { 
+        vectors2.sort(() => Math.random() - Math.random());
+    }
+    if (key == 'r'){
+        setup();
+    }
+}
